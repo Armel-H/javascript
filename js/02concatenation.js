fc3cbdee58acf9f467bc83a88fd25fd75887cdfc
Unknown <sandra.herisson@lepoles.com>
2017-07-13 16:30:21 +0200

    13072017

---
+++ js/02concatenation.js
@@ -0,0 +1,28 @@
+//alert("H1");
+
+/*-----------
+LA CONCATENATION
+-------------*/
+var annee = 2017;
+var futur =  3;
+var calcul = annee+futur;
+console.log(calcul);
+document.write(calcul+"<br>");
+
+var mois = "7";
+var calcul2 = annee+mois;
+console.log(calcul2);
+document.write(calcul2+"<br>");
+
+var DebutPhrase = "Aujourd'hui ";
+var NbStagiaires = 12;
+var SuitePhrase = " stagiaires";
+var FinPhrase = " sont présents.";
+
+var calcul3 = DebutPhrase + NbStagiaires + SuitePhrase + FinPhrase;
+console.log(calcul3);
+document.write(calcul3+"<br>");
+
+//--Nous souhaitons afficher la phrase en un seul morceau avec la cvoncaténation => à vos clavier!--//
+//mettre des espaces
+//document.write(DebutPhrase + NbStagiaires + SuitePhrase + " " +FinPhrase);
